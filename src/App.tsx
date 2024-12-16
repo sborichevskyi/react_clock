6def27b16e5d6ca89bd72e0cf67e35e466f535c6
Stas Borychevskyi <sborichevskiy74@gmail.com>
2024-12-16 11:26:40 +0200

    Solution2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { Clock } from './clock';
+import { Clock } from './components/Clock';
 
 // export const App: React.FC = () => {
 //   return (
